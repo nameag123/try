907080dc7bdcd2c5fa31b1192cb84c40a85ee802
glac <858884967@qq.com>
2026-05-19 21:50:10 +0800

    testing

---
+++ a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve() {
+    //1
+    return ;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
